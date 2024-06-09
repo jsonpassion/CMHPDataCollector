b07884c12c991df6bc8c2faa4040773ddaacb8f2
Jason_ADA <iamjason@g.postech.edu>
2024-06-10 00:41:01 +0900

    Initial Commit

---
+++ CMHPDataCollector/CMHPDataCollectorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CMHPDataCollectorApp.swift
+//  CMHPDataCollector
+//
+//  Created by Admin on 6/10/24.
+//
+
+import SwiftUI
+
+@main
+struct CMHPDataCollectorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
